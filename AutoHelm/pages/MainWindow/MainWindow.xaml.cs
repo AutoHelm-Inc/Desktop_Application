2b2c05c7ab49128a6597aaecc5ce24fcad79ae3a
Zaid Omer <z2omer@uwaterloo.ca>
2023-07-04 05:14:54 -0400

    Restructure project with 'pages' folder

--- AutoHelm/pages/MainWindow/MainWindow.xaml.cs
+++ AutoHelm/pages/MainWindow/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AutoHelm
+namespace AutoHelm.pages.MainWindow
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
